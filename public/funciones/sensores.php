851729ee265485b348b6b9fe96e51f0a8e8765f8
Aline4555445 <67912626+Aline4555445@users.noreply.github.com>
2020-07-20 16:58:37 -0500

    api para el proyecto invernadero

---
+++ public/funciones/sensores.php
@@ -0,0 +1,21 @@
+<?
+require __DIR__ . '/../../src/models/sensores.php';
+function funcionsensores($request){
+    $objSensor= new Sensores();
+    return $objSensor->insertarSensor($request);
+}
+function funciongetSensoreData($request){
+    $objSensor= new Sensores();
+    return $objSensor->getSensorData($request);
+}
+//eliminar
+function funcionEliminarsensores($request){
+    $objSensor= new Sensores();
+    return $objSensor->eliminarSensor($request);
+}
+
+//actualizar
+function funcionActualizarsensores($request){
+    $objSensor= new Sensores();
+    return $objSensor->actualizarSensor($request);
+}
